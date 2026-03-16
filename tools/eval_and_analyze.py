1377c0a6bc1511bc4177819adfd94e9c3f042486
PsychoKiller27 <42900905+BIT-LiuLang@users.noreply.github.com>
2026-03-16 19:54:02 +0800

    Update import path for GEODMS

--- tools/eval_and_analyze.py
+++ tools/eval_and_analyze.py
@@ -20,7 +20,7 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 
-from geo_dms.models.meta_arch.sam3d_body import GEODMS
+from geo_dms.models.meta_arch.geo_dms import GEODMS
 from geo_dms.utils.config import get_config
 from geo_dms.utils.checkpoint import load_state_dict
 from geo_dms.data.datasets.dms_datasets import get_dms_loader
